5fa6611b5eaac730c0bad62f0e62f407e5f2fcfc
Georgia <ghobs987@gmail.com>
2022-03-31 15:56:28 +0100

    landing boiler plate

--- 5874/src/components/Landing.js
+++ 5874/src/components/Landing.js
@@ -1,5 +1,21 @@
 import React from "react";
-
+import "./Landing.css";
 export default function Footer() {
-  return <div></div>;
+  return (
+    <div className="landing-container">
+      <div className="intro-container">
+        <div className="logo-div"></div>
+        <div className="brand-statement-container">
+          <p className="key-word"></p>
+          <h2 className="title-1"></h2>
+          <h2 className="title-2"></h2>
+          <p className="brand-statement"></p>
+          <a className="talk" href="#">
+            lets talk
+          </a>
+        </div>
+      </div>
+      <div className="img-container"></div>
+    </div>
+  );
 }
