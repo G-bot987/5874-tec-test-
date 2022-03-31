fc64808f7d5ebd9313039b198574979394a80456
Georgia <ghobs987@gmail.com>
2022-03-31 23:26:28 +0100

    landing render

--- 5874/src/components/Landing.js
+++ 5874/src/components/Landing.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./Landing.css";
+import backdrop from "../imgs/Spaniel01_gradient.png";
+import logo from "../imgs/Digital-Spaniel-logo01-01.png";
 export default function Footer() {
   return (
     <div className="landing-container">
-      <div className="intro-container">
-        <div className="logo-div"></div>
-        <div className="brand-statement-container">
-          <p className="key-word"></p>
-          <h2 className="title-1"></h2>
-          <h2 className="title-2"></h2>
-          <p className="brand-statement"></p>
-          <a className="talk" href="#">
-            lets talk
-          </a>
+      <div className="left-container-landing">
+        <img src={logo} className="logo" alt="Digital Spaniel Logo" />
+        <div className="intro-container">
+          <div className="brand-statement-container">
+            <p className="key-word"> BRAND, DEV, ECOM, MARKETING</p>
+            <h2 className="title-1">We unleash </h2>
+            <h2 className="title-2"> business potential</h2>
+
+            <p className="brand-statement">
+              We create brand experiences which are memorable and distinct. Our
+              experienced team create and develop brands with personality and
+              resonance.
+            </p>
+
+            <a className="talk" href="#">
+              lets talk
+            </a>
+          </div>
         </div>
       </div>
-      <div className="img-container"></div>
+      <img
+        src={backdrop}
+        className="backdrop"
+        alt="Running Spaniel with a blue backdrop"
+      />
     </div>
   );
 }
