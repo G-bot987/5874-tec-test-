e19b3c2dcfb4a69d9fe04de681c0f81e8285794e
Georgia <ghobs987@gmail.com>
2022-03-31 15:55:58 +0100

    nav, landing render

--- 5874/src/App.js
+++ 5874/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <Landing />
       <Footer />
     </div>
