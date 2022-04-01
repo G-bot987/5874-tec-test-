8336bce27cc2030fe6f73c47df7a2ac2e9a61d66
Georgia <ghobs987@gmail.com>
2022-04-01 07:54:24 +0100

    burger comp

--- 5874/src/components/Nav.js
+++ 5874/src/components/Nav.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Rightnav from "./Rightnav";
 import "./Nav.css";
-export default function Footer() {
-  return <div className="nav-container">i am nav</div>;
-}
+import Burger from "./Burger";
+
+export const Nav = () => {
+  return (
+    <div className="nav-container">
+      <Burger />
+    </div>
+  );
+};
+
+export default Nav;
